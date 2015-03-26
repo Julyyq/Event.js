945f0c382fb80c9a1200a9974031cc71c3b35719
Julyyq <colorfuljuly@gmail.com>
2015-03-26 11:58:35 +0800

    add comment

--- Event.js
+++ Event.js
@@ -2,6 +2,10 @@ var Event = (function() {
   var Event = {},eventListners={};
   Event = {
     emit: function(eventName) {
+			// arguments.callee指当前的调用函数
+			// arguments.callee.length指形参数量
+			// js中function.length指形参数量，arguments.length指实参数量
+			// 这里不应该传形参，当时应该写错了todo
       var theArgs = Array.prototype.slice.call(arguments,arguments.callee.length);
       eventListners[eventName].apply(this,theArgs);
     },
